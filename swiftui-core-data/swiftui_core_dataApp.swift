bafdf0577bfcdf7f1f45560a1599d6c59bab8b39
ceyhun-celik <ceyhuniverse@gmail.com>
2023-02-12 09:29:44 +0300

    Initial Commit

---
+++ swiftui-core-data/swiftui_core_dataApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_core_dataApp.swift
+//  swiftui-core-data
+//
+//  Created by Ceyhun Çelik on 12.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_core_dataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
